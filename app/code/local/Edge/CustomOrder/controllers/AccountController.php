31efaa3e141bf71bda60e2019b9eb48c15d421ec
Victor Seager <vseager@gmail.com>
2017-02-09 18:29:29 +0000

    Removed commented out code

--- app/code/local/Edge/CustomOrder/controllers/AccountController.php
+++ app/code/local/Edge/CustomOrder/controllers/AccountController.php
@@ -44,38 +44,4 @@ class Edge_CustomOrder_AccountController extends Mage_Customer_AccountController
         }
         $this->_redirectUrl($session->getBeforeAuthUrl(true));
     }
-    
-    
-    /**
-     * Define target URL and redirect customer after logging in
-     */
-//    protected function _loginPostRedirect()
-//    {
-//        $session = $this->_getSession();
-//        
-//        // Set default URL to redirect customer to
-//        $session->setBeforeAuthUrl($this->_getHelper('customer')->getAccountUrl());
-//        // Redirect customer to the last page visited after logging in
-//        if ($session->isLoggedIn()) {
-//            if (!Mage::getStoreConfigFlag(
-//                Mage_Customer_Helper_Data::XML_PATH_CUSTOMER_STARTUP_REDIRECT_TO_DASHBOARD
-//            )) {
-//                $referer = $this->getRequest()->getParam(Mage_Customer_Helper_Data::REFERER_QUERY_PARAM_NAME);
-//                if ($referer) {
-//                    // Rebuild referer URL to handle the case when SID was changed
-//                    $referer = $this->_getModel('core/url')
-//                        ->getRebuiltUrl( $this->_getHelper('core')->urlDecodeAndEscape($referer));
-//                    if ($this->_isUrlInternal($referer)) {
-//                        $session->setBeforeAuthUrl($referer);
-//                    }
-//                }
-//            } else if ($session->getAfterAuthUrl()) {
-//                $session->setBeforeAuthUrl($session->getAfterAuthUrl(true));
-//            }
-//        } else {
-//            $session->setBeforeAuthUrl( $this->_getHelper('customer')->getLoginUrl());
-//        }
-//        
-//        $this->_redirectUrl($session->getBeforeAuthUrl(true));
-//    }
 }
